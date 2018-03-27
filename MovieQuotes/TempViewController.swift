9a861506f5d272846a1602b2efe009fa733d74ae
CSSE Department <csse@cssemm-7.reshall.rose-hulman.edu>
2018-03-27 12:32:37 -0400

    Temp

---
+++ MovieQuotes/TempViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TempViewController.swift
+//  MovieQuotes
+//
+//  Created by CSSE Department on 3/27/18.
+//  Copyright © 2018 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class TempViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TempCell", for: indexPath)
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
